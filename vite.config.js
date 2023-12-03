d62b5433a4c107b86cf146abe8e55a296764b404
Gabriel de Jesus <devgabrieldejesus@gmail.com>
2023-12-03 16:46:01 -0300

    fix: fix error in the build on linux

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1600,
-  },
-});
+  build: { chunkSizeWarningLimit: 1600 },
+})
